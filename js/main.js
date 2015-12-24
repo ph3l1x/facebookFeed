6a184f91a2bd3b4a38c652a1a484c48306857488
ph3l1x <rperry@mindimage.net>
2015-12-24 03:17:11 -0700

    Login Works

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-$("document").ready(function () {
+tizen.power.request("SCREEN", "SCREEN_NORMAL");
 
-    /***************************************************************
-     * Check if logged in and preset authentication process if not *
-     ***************************************************************/
+$("document").ready(function () {
 
     var appID = '896778317084638';
 
@@ -19,7 +17,6 @@ $("document").ready(function () {
             data: formData
         });
     }
-
     $("#button-click").click(function () {
         var formData = {
             type: "device_code",
@@ -33,49 +30,65 @@ $("document").ready(function () {
             url: "https://graph.facebook.com/oauth/device",
             data: formData,
             success: function (data) {
-                $('.button-container').html(
-                    '<div class="test">' +
-                    '  <div class="url">Login URL: <a target="_BLANK" href="' + data.verification_uri + '">' + data.verification_uri + '</a></div>' +
-                    '  <div class="code">Actication Code: ' + data.user_code + '</div>' +
-                    '  <div class="expires">Expires in: ' + data.expires_in / 60 + ' minutes</div>' +
+                localStorage['code'] = data.code;
+            	$('#button-container').html(
+                    '<div class="fbReturn">' +
+                    '  <div class="description">' +
+                    '       You must validate your account. ' +
+                    '       From another device make sure that you are logged into your facebook account and visit: <div>' +
+                            data.verification_uri +
+                    '       </div>and type the activation code: <div>' +
+                            data.user_code +
+                    '       </div>This code will expire in: <div>' +
+                            data.expires_in / 60 +
+                    '       minutes</div>' +
                     '</div>'
                 );
                 var times = data.expires_in / 5,
                     runTime = 0,
-                    token = '';
-
-                var interval = setInterval(function () {
-                    if (runTime < times) {
-                        authPoll(data).then(function (returnedInfo) {
-                            var token = returnedInfo.access_token,
-                                expires_in = returnedInfo.expires_in;
-                            if (token) {
-                                clearInterval(interval);
-                                $('.button-container').html(
-                                    '<div class="test">' +
-                                    '  <div class="token">Token: ' + token + '</div>' +
-                                    '  <div class="expiresIn">Expires In: ' + expires_in + '</div>' +
-                                    '</div>'
-                                );
-                            }
-                        });
-                        runTime += 1;
-                    } else {
-                        clearInterval(interval);
-                    }
-                }, 5000);
-
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                console.log("ERROR");
+                    interval = setInterval(function () {
+                        if (runTime < times) {
+                            authPoll(data).then(function (returnedInfo) {
+                                var token = returnedInfo.access_token;
+                                    //expires_in = returnedInfo.expires_in;
+                                if (token) {
+                                    clearInterval(interval);
+                                    localStorage['accesstoken'] = token;
+                                    authData = {
+                                        fields: "name,picture",
+                                        access_token: localStorage['accesstoken']
+                                    };
+                                    $.ajax({
+                                        type: "GET",
+                                        dataType: "json",
+                                        url: "https://graph.facebook.com/v2.3/me",
+                                        data: authData,
+                                        success: function(data) {
+                                            localStorage['uid'] = data.id;
+                                            localStorage['name'] = data.name;
+                                            localStorage['pictureUrl'] = data.picture.data.url;
+                                            $('#button-container').html(
+                                                '<div class="fbReturn">' +
+                                                '  <div class="success"><h2>Success!</h2></div>' +
+                                                '  <div class="successContainer">' +
+                                                '       <div class="userPicture"><img src="' + localStorage['pictureUrl'] + '"></div>' +
+                                                '       <div class="userRight">' +
+                                                '          <div class="userName">' + localStorage['name'] + ',</div>' +
+                                                '          <div class="userText">You are now logged in.</div>' +
+                                                '       </div>' +
+                                                '       <div class="continueButton">Continue</div>' +
+                                                '</div>'
+                                            )
+                                        }
+                                    });
+                                }
+                            });
+                            runTime += 1;
+                        } else {
+                            clearInterval(interval);
+                        }
+                    }, 5000);
             }
-        })
-
+        });
     });
-
-    /******************************************************
-     * Show Facebook Info after successful Authentication *
-     ******************************************************/
-    console.log(window.fbToken);
-    console.log(window.expiresIn);
 });
